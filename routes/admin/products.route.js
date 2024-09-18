d6ec31bacf17ad53303a465353b6b5a056b1c9ef
Dinh Nhu Khai <khainhatao@gmail.com>
2024-09-18 13:00:01 +0700

    update deploy

--- routes/admin/products.route.js
+++ routes/admin/products.route.js
@@ -2,9 +2,12 @@ const controllerAdmin = require("../../controllers/admin/products.controller");
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
 const uploadName = require("../../helpers/upload");
+// const upload = multer({ storage: uploadName() }); //cai nay danh cho local chu kphai server
 const validate = require("../../validate/admin/products.validate");
-const upload = multer({ storage: uploadName() });
+const upload = multer();
 
 router.get("/", controllerAdmin.product);
 router.patch("/change-status/:status/:id", controllerAdmin.changeStatus);
@@ -15,6 +18,32 @@ router.get("/create", controllerAdmin.create);
 router.post(
   "/create",
   upload.single("thumbnail"),
+  function (req, res, next) {
+    if (req.file) {
+      let streamUpload = (req) => {
+        return new Promise((resolve, reject) => {
+          let stream = cloudinary.uploader.upload_stream((error, result) => {
+            if (result) {
+              resolve(result);
+            } else {
+              reject(error);
+            }
+          });
+
+          streamifier.createReadStream(req.file.buffer).pipe(stream);
+        });
+      };
+
+      async function upload(req) {
+        let result = await streamUpload(req);
+        console.log(result);
+        upload(req);
+        next();
+      }
+    } else {
+      next();
+    }
+  },
   validate.createPost, //trung gian middleware, check r mới tới controller tiếp theo
   controllerAdmin.createPost
 );
